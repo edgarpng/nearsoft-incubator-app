81ec559f97632714c03b4d4c489e1ed01a7d61ad
Edgar <edgar.png@gmail.com>
2014-07-09 20:40:23 -0700

    Remove alert html creation logic from javascript file to a template

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -64,7 +64,7 @@
     });
     App.ApplicationRoute = Ember.Route.extend({
       setupController: function(controller){
-        controller.set('model', App.Search.create());
+        controller.set('model', window.search = App.Search.create());
       }
     });
     App.SearchRoute = Ember.Route.extend({
@@ -84,7 +84,6 @@
           var search = this.get('model');
           if(search.get('isValid')){
             this.doSearch();
-            this.trigger('searchValid');
           }
           else{
             this.trigger('searchInvalid');
@@ -115,31 +114,21 @@
     //Ember Views
     App.ApplicationView = Ember.View.extend({
       didInsertElement: function(){
-        this.hideNiceAlert();
-        this.get('controller').on('searchInvalid', this, this.showNiceAlert);
-        this.get('controller').on('searchValid', this, this.hideNiceAlert);
-        this.get('controller').on('serviceError', this, this.showServiceError);
+        //Model is invalid the moment after its creation, 
+        this.hideSearchInvalidAlert();
+        this.get('controller').on('searchInvalid', this, this.showSearchInvalidAlert);
+        this.get('controller').on('serviceError', this, this.showServiceErrorAlert);
       },
-      showNiceAlert: function(){
-        var message = 'Please check the following errors:';
-        var errors = this.get('controller.errors');
-        var property;
-        message += '<ul>';
-        for(property in errors){
-          if(errors.hasOwnProperty(property) && errors[property].length){
-            message += '<li>' + errors[property] + '</li>';
-          }
-        }
-        message += '</ul>';
-        this.$('.alert').show().find('.alert-content').html(message);
+      showSearchInvalidAlert: function(){
+        this.$('.alert').show();
       },
-      hideNiceAlert: function(){
+      hideSearchInvalidAlert: function(){
         this.$('.alert').hide();
       },
-      showServiceError: function(){
+      showServiceErrorAlert: function(){
         alert('Something went awfully wrong with the service. Please try again.');
       }
-    })
+    });
 
     //Ember Models
     App.Search = Ember.Object.extend(Ember.Validations.Mixin, {
@@ -156,22 +145,39 @@
         arrivalDate: {
           presence: {message: 'Arrival date cannot be blank'}
         },
-        areDatesValid: {
+        datesAreValid: {
           acceptance: {
-            if: 'areDatesPresent',
             message: 'Arrival date must after or the same as the departure'
           }
         }
       },
-      areDatesValid: function(){
+      observeDates: function(){
+        //Validator relies on Ember setters, so custom validation properties must be explicitly set
         var departure = this.get('departureDate');
         var arrival = this.get('arrivalDate');
-        var difference = moment(arrival).diff(moment(departure));
-        return difference >= 0;
-      }.property('departureDate', 'arrivalDate'),
-      areDatesPresent: function(){
-        return this.get('departureDate') && this.get('arrivalDate');
-      }.property('departureDate', 'arrivalDate'),
+        var timeDifference = moment(arrival).diff(moment(departure));
+        var datesArePresent = departure !== undefined && arrival !== undefined
+          && departure.length > 0 && arrival.length > 0;
+        var datesAreValid;
+        if(!datesArePresent){
+          //Only validate correctness if both dates are present
+          datesAreValid = true;
+        }
+        else{
+          datesAreValid = timeDifference >= 0;
+        }
+        this.set('datesAreValid', datesAreValid);
+      }.observes('departureDate', 'arrivalDate'),
+      errorMessages: function(){
+        var errorMessages = [];
+        var errors = this.get('errors'), error;
+        for(error in errors){
+          if(errors.hasOwnProperty(error) && errors[error].length){
+            errorMessages = errorMessages.concat(errors[error]);
+          }
+        }
+        return errorMessages;
+      }.property('errors.departureDate', 'errors.arrivalDate', 'errors.departureAirport', 'errors.arrivalAirport', 'errors.datesAreValid'),
       queryString: function(){
         return Ember.$.param({
           departureAirport: this.get('departureAirport'),
